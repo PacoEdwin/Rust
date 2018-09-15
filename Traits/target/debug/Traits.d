c4e48700faffb3820b98d7b46dee7468b491e877
Эдвин Пако <edwinswatpako@gmail.com>
2018-09-15 22:01:37 +0300

    Traits

---
+++ Traits/target/debug/Traits.d
@@ -0,0 +1,1 @@
+C:\Users\Edwin\Documents\Rust\Traits\target\debug\Traits.pdb: C:\Users\Edwin\Documents\Rust\Traits\src\main.rs
---
+++ Traits/target/debug/deps/Traits-21626946d13a14d5.d
@@ -0,0 +1,5 @@
+C:\Users\Edwin\Documents\Rust\Traits\target\debug\deps\Traits-21626946d13a14d5.exe: src\main.rs
+
+C:\Users\Edwin\Documents\Rust\Traits\target\debug\deps\Traits-21626946d13a14d5.d: src\main.rs
+
+src\main.rs:
