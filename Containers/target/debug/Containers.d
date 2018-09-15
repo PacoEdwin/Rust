865f51f5a0a93e09615e57e9abcd54e66f99ebe7
Эдвин Пако <edwinswatpako@gmail.com>
2018-09-15 22:59:32 +0300

    Enums, Error processing, Containers

---
+++ Containers/target/debug/Containers.d
@@ -0,0 +1,1 @@
+C:\Users\Edwin\Documents\Rust\Containers\target\debug\Containers.pdb: C:\Users\Edwin\Documents\Rust\Containers\src\main.rs
---
+++ Containers/target/debug/deps/Containers-d3f3892d9fe4e17d.d
@@ -0,0 +1,5 @@
+C:\Users\Edwin\Documents\Rust\Containers\target\debug\deps\Containers-d3f3892d9fe4e17d.exe: src\main.rs
+
+C:\Users\Edwin\Documents\Rust\Containers\target\debug\deps\Containers-d3f3892d9fe4e17d.d: src\main.rs
+
+src\main.rs:
