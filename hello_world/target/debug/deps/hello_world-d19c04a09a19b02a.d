9e062bd77404e9aa74c7b6f289e1b320575d19f7
Эдвин Пако <edwinswatpako@gmail.com>
2018-09-15 16:59:02 +0300

    Struct consturctor simulation

---
+++ hello_world/target/debug/deps/hello_world-d19c04a09a19b02a.d
@@ -0,0 +1,5 @@
+C:\Users\Edwin\Documents\Rust\hello_world\target\debug\deps\hello_world-d19c04a09a19b02a.exe: src\main.rs
+
+C:\Users\Edwin\Documents\Rust\hello_world\target\debug\deps\hello_world-d19c04a09a19b02a.d: src\main.rs
+
+src\main.rs:
